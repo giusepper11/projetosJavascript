629b9455264c61142967377b3902605b9645190d
Giuseppe Rosa <giusepper11@gmail.com>
2019-08-12 16:03:30 -0300

    Manipulando elementos

---
+++ usuarios/index.js
@@ -0,0 +1,13 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+
+var nome = $('#exampleInputName');
+var gender = $('#exampleInputName');
+var gender = $('#exampleInputName');
+var birth = $('#exampleInputName');
+var country = $('#exampleInputName');
+var email = $('#exampleInputName');
+var password = $('#exampleInputName');
+var photo = $('#exampleInputName');
+var admin = $('#exampleInputName');
