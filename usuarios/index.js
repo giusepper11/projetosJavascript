7ce1034d6c3e2fbc8958a9ff04c0b6a7bdd607fa
Giuseppe Rosa <giusepper11@gmail.com>
2019-08-12 17:59:06 -0300

    Seletores

--- usuarios/index.js
+++ usuarios/index.js
@@ -3,11 +3,10 @@ const $$ = document.querySelectorAll.bind(document);
 
 
 var nome = $('#exampleInputName');
-var gender = $('#exampleInputName');
-var gender = $('#exampleInputName');
-var birth = $('#exampleInputName');
-var country = $('#exampleInputName');
-var email = $('#exampleInputName');
-var password = $('#exampleInputName');
-var photo = $('#exampleInputName');
-var admin = $('#exampleInputName');
+var gender = $$('#form-user-create [name=gender]:checked');
+var birth = $('#exampleInputBirth');
+var country = $('#exampleInputCountry');
+var email = $('#exampleInputEmail');
+var password = $('#exampleInputPassword');
+var photo = $('#exampleInputFile');
+var admin = $('#exampleInputAdmin');
